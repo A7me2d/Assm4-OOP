e447a34f88e71a97f5c8e05c0288b4b38d3c8d63
A7me2d <152171738+A7me2d@users.noreply.github.com>
2024-07-16 20:12:37 +0300

    Interfaces

--- Assm4 OOP/Program.cs
+++ Assm4 OOP/Program.cs
@@ -1,10 +1,61 @@
-﻿namespace Assm4_OOP
+﻿using Assm4_OOP.Interfaces;
+
+namespace Assm4_OOP
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+
+            #region Interfaces
+
+            //IMyType myType = new IMyType();
+
+            //IMyType myType;
+
+            //myType.Id = 20;
+            //myType.MyFyn();
+            //myType.Print();
+
+
+            //MyType myType1 = new MyType();
+
+            //myType1.Id = 30;
+            //myType1.MyFyn(100);
+            //myType1.Print();
+
+            IMyType myType = new MyType();
+
+            myType.Id = 1;
+            myType.MyFyn(800);
+            myType.Print();
+
+
+
+            #endregion
+
+            #region
+
+            #endregion
+
+            #region
+
+            #endregion
+
+            #region
+
+            #endregion
+
+            #region
+
+            #endregion
+
+            #region
+
+            #endregion
+
+
         }
     }
 }
