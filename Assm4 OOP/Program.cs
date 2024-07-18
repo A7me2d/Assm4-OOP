ec81e40d7d216a2fa4343c6d6b36fb00c8472cdc
A7me2d <152171738+A7me2d@users.noreply.github.com>
2024-07-18 19:26:08 +0300

    Part01

--- Assm4 OOP/Program.cs
+++ Assm4 OOP/Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Part01
+            //Q1 ======> (b);
+            //Q2 ======> (a , b);
+            //Q3 ======> (b);
+            //Q4 ======> (b);
+            //Q5 ======> (d);
+            //Q6 ======> (a);
+            //Q7 ======> (b);
+            //Q8 ======> (a);
+            //Q9 ======> (b);
+            //Q10 ======> (c);
+            #endregion
         }
     }
 }
